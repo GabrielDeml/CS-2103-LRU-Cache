a18cbfd20e141cc49e60646f664531b24ccdb9b5
GregoryConrad <gregorysconrad@gmail.com>
2019-11-05 21:47:53 -0500

    Cleaned up tests

--- test/QueueTest.java
+++ test/QueueTest.java
@@ -2,7 +2,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * A class to test the ConstantTimeQueue
+ */
 public class QueueTest {
+    /**
+     * Tests the enqueue functionality
+     */
     @Test
     public void testEnqueue() {
         ConstantTimeQueue<Character> queue = new ConstantTimeQueue<>();
@@ -10,35 +16,34 @@ public class QueueTest {
         assertEquals("a, b, c, d, e", queue.toString());
     }
 
-    // todo test dequeue
+    /**
+     * Tests the dequeue functionality
+     */
     @Test
     public void testDequeue() {
         // Check when head is empty
         ConstantTimeQueue<Integer> keys = new ConstantTimeQueue<>();
         assertNull(keys.dequeue());
 
-        // Check when there is only one object
-        keys = new ConstantTimeQueue<>();
-        keys.enqueue(0);
-        keys.dequeue();
-        assertNull(keys.getFirst());
-
-        // Check when there is two objects
-        keys = new ConstantTimeQueue<>();
-//        keys.enqueue(0);
-//        keys.enqueue(1);
-        putObjectsInQueue(2, keys);
-        int dequeued = keys.dequeue();
-        assertEquals((int) keys.getFirst(), 1);
-        assertEquals(dequeued, 0);
+        // Check for a variable amount of objects (from 1 to 10)
+        for (int i = 1; i < 10; ++i) {
+            putObjectsInQueue(i, keys);
+            for (int j = 0; j < i; ++j) {
+                assertEquals(j, (int) keys.getFirst());
+                assertEquals(j, (int) keys.dequeue());
+            }
+            assertNull(keys.getFirst());
+        }
     }
 
+    /**
+     * Tests the remove functionality
+     */
     @Test
     public void testRemove() {
         ConstantTimeQueue<Integer> keys = new ConstantTimeQueue<>();
 
         // Make sure we can get rid of everything
-        keys = new ConstantTimeQueue<>();
         putObjectsInQueue(10, keys);
         for (int i = 0; i < 10; i++) {
             keys.remove(i);
@@ -46,7 +51,6 @@ public class QueueTest {
         assertNull(keys.getFirst());
 
         // Get rid of the first half
-        keys = new ConstantTimeQueue<>();
         putObjectsInQueue(10, keys);
         for (int i = 0; i < 5; i++) {
             keys.remove(i);
@@ -67,13 +71,15 @@ public class QueueTest {
             keys.remove(i);
         }
         assertNull(keys.getLast());
-
-
     }
 
+    /**
+     * A helper method that fills up a queue with varying integers
+     *
+     * @param n the upper bound of integers to add to the queue
+     * @param q the queue to fill with integers
+     */
     private void putObjectsInQueue(int n, ConstantTimeQueue<Integer> q) {
-        for (int i = 0; i < n; i++) {
-            q.enqueue(i);
-        }
+        for (int i = 0; i < n; i++) q.enqueue(i);
     }
 }
